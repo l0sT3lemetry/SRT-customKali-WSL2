53505238cba160246b1d4c8b9ceb193fdfff922c
l0sT3lemetry <145846586+l0sT3lemetry@users.noreply.github.com>
2023-11-18 20:24:32 +0900

    Create SRT-install.ps1
    Your download of the rootFS is going to fail. Check the code. For now this is for me.

---
+++ SRT-install.ps1
@@ -0,0 +1,42 @@
+# A script to install my custom kali WSL instance on LP+
+<# Pre-reqs: 
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+Restart-Computer
+
+root CA installed
+
+the ability to download
+#>
+
+Write-Host "I hope you won't regret this... installing."
+
+mkdir C:\kali
+
+cd \kali
+
+Invoke-Webrequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile wsl_update_x64.msi
+Invoke-Item .\wsl_update_x64.msi
+Write-Host "Complete the install before continuing."
+Pause
+Remove-Item .\wsl_update_x64.msi
+
+wsl --set-default-version 2
+
+Write-Host "Downloading root filesystem and LxRunOffline, it's up to you to trust these files."
+# Add dl=1 to download from cmd line when using dropbox
+iwr https://www.dropbox.com/s/<redacted>/kali-linux-rolling-wsl-rootfs-amd64.tar.gz?dl=1 -o kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+iwr https://github.com/DDoSolitary/LxRunOffline/releases/download/v3.5.0/LxRunOffline-v3.5.0-msvc.zip -o lxrun.zip
+
+Expand-Archive .\lxrun.zip
+
+.\lxrun\LxRunOffline.exe install -n kali-WSL2 -d .\kali\ -f .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+
+# more cleanup
+Remove-Item .\lxrun -Recurse
+Remove-Item .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+
+wsl --list --verbose
+Write-Host "You should see kali above and in a stopped state if this script worked."

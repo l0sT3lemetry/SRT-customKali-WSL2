cb13418e3bccd3ce3edb656ffccc284adbca99a1
l0sT3lemetry <145846586+l0sT3lemetry@users.noreply.github.com>
2023-11-22 00:14:18 +0900

    Update SRT-install.ps1

--- SRT-install.ps1
+++ SRT-install.ps1
@@ -1,15 +1,13 @@
 # A script to install my custom kali WSL instance using a rootFS on LP+
-<# Pre-reqs: 
+<# Pre-reqs:
+Run the following commands in an elevated prompt:
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
 Restart-Computer
 
-root CA installed
-
-the ability to download
+Connect to platform, and enable the ability to download.
 #>
+
 $rootFS = "https://www.dropbox.com/s/<redacted>/kali-linux-rolling-wsl-rootfs-amd64.tar.gz?dl=1"
 $outFile = "kali-linux-rolling-wsl-rootfs-amd64.tar.gz"
 $kaliHash = "8A8BDCE93B535831E02AF141379EB3FBAC3C811310904180A6C135719B7683FD"
@@ -21,9 +19,8 @@ Invoke-Webrequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl
 Invoke-Item .\wsl_update_x64.msi
 Write-Host "Complete the install before continuing."
 Pause
-Remove-Item .\wsl_update_x64.msi
 
-Write-Host "Attempting to set WSL Version to 2."
+Write-Host "Setting Default WSL Version to 2."
 wsl.exe --set-default-version 2
 
 # This speeds up downloads in certain versions of powershell
@@ -32,7 +29,7 @@ Write-Host "`nDownloading root filesystem. Progress will not be displayed to imp
 # Add dl=1 to download from cmd line when using dropbox
 iwr $rootFS -o $outFile
 $ProgressPreference = 'Continue'
-$dlHash = Get-FileHash -Algorithm sha256 .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz | select Hash
+$dlHash = Get-FileHash -Algorithm sha256 $outFile | select Hash
 Write-Host "Original file hash: " $kaliHash
 Write-Host "Download file hash: " $dlHash.Hash
 if ( $kaliHash -eq $dlHash.Hash ) { Write-Host "Match." -ForegroundColor Green }
@@ -41,19 +38,15 @@ Else { "Download Corrupt or you didn't change the original file hash." -Foregrou
 Write-Host "Downloading LxRunOffline.`n"
 iwr https://github.com/DDoSolitary/LxRunOffline/releases/download/v3.5.0/LxRunOffline-v3.5.0-msvc.zip -o lxrun.zip
 Expand-Archive .\lxrun.zip
-.\lxrun\LxRunOffline.exe install -n kali-SRT -d .\kali-SRT\ -f .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+.\lxrun\LxRunOffline.exe install -n kali-SRT -d .\kali-SRT\ -f .\$outFile
 
-# More cleanup
+Remove-Item .\wsl_update_x64.msi
 Remove-Item .\lxrun.zip
 Remove-Item .\lxrun -Recurse
-Remove-Item .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
-
+Remove-Item $outFile
 
 Write-Host "Converting to WSL version 2."
-
-# Comment out this line if you prefer WSL 1, it will get you started considerably faster
+# Comment out the next line if you prefer WSL 1, it will get you started considerably faster
 wsl --set-version kali-SRT 2
-
 wsl --list --verbose
-
-Write-Host "`nYou should see kali above and in a stopped state if this script worked."
+Write-Host "`nYou should see kali above and in a stopped state if this script worked." -ForegroundColor Cyan

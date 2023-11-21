632cabb34a0fdff5405dcd2bcb60acf6a75a0b35
l0sT3lemetry <145846586+l0sT3lemetry@users.noreply.github.com>
2023-11-21 22:54:21 +0900

    Update SRT-install.ps1

--- SRT-install.ps1
+++ SRT-install.ps1
@@ -1,4 +1,4 @@
-# A script to install my custom kali WSL instance on LP+
+# A script to install my custom kali WSL instance using a rootFS on LP+
 <# Pre-reqs: 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
@@ -12,42 +12,35 @@ the ability to download
 #>
 $kaliHash = "8A8BDCE93B535831E02AF141379EB3FBAC3C811310904180A6C135719B7683FD"
 
-Write-Host "Attempting to install."
+Write-Host "Downloading WSL Kernel.`n"
 cd ~\desktop
 
 Invoke-Webrequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile wsl_update_x64.msi
-Start-Sleep -Seconds 1
 Invoke-Item .\wsl_update_x64.msi
 Write-Host "Complete the install before continuing."
 Pause
-#Remove-Item .\wsl_update_x64.msi
+Remove-Item .\wsl_update_x64.msi
 
-Start-Sleep -Seconds 1
-
+Write-Host "Attempting to set WSL Version to 2.`n"
 wsl.exe --set-default-version 2
 
-Write-Host "Downloading root filesystem."
+$ProgressPreference = 'SilentlyContinue'
+Write-Host "Downloading root filesystem. Progress will not be displayed to improve download speed.`n"
 # Add dl=1 to download from cmd line when using dropbox
 iwr https://www.dropbox.com/s/<redacted>/kali-linux-rolling-wsl-rootfs-amd64.tar.gz?dl=1 -o kali-linux-rolling-wsl-rootfs-amd64.tar.gz
 $dlHash = Get-FileHash -Algorithm sha256 .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
-$kaliHash
-$dlHash.Hash
+Write-Host "Original file hash: $kaliHash"
+Write-Host "Download file hash: $dlHash.Hash"
 
-Write-Host "Downloading LxRunOffline."
+Write-Host "Downloading LxRunOffline.`n"
 iwr https://github.com/DDoSolitary/LxRunOffline/releases/download/v3.5.0/LxRunOffline-v3.5.0-msvc.zip -o lxrun.zip
-
-Start-Sleep -Seconds 1
-
 Expand-Archive .\lxrun.zip
-
-Start-Sleep -Seconds 1
-
 .\lxrun\LxRunOffline.exe install -n kali -d .\kali\ -f .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
 
-# more cleanup
+# More cleanup
 Remove-Item .\lxrun.zip
 Remove-Item .\lxrun -Recurse
 Remove-Item .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
 
 wsl --list --verbose
-Write-Host "You should see kali above and in a stopped state if this script worked."
+Write-Host "You should see kali above and in a stopped state if this script worked. Quite possibly even if it didn't.."

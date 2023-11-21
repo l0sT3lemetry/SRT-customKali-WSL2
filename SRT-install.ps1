0bf3f1854e45af791438e6373c54581761bead88
l0sT3lemetry <145846586+l0sT3lemetry@users.noreply.github.com>
2023-11-22 01:28:10 +0900

    Update SRT-install.ps1

--- SRT-install.ps1
+++ SRT-install.ps1
@@ -7,28 +7,24 @@ Restart-Computer
 
 Connect to platform, and enable the ability to download.
 #>
-
+# This speeds up downloads in certain versions of powershell
+$ProgressPreference = 'SilentlyContinue'
 $rootFS = "https://www.dropbox.com/s/<redacted>/kali-linux-rolling-wsl-rootfs-amd64.tar.gz?dl=1"
 $outFile = "kali-linux-rolling-wsl-rootfs-amd64.tar.gz"
 $kaliHash = "8A8BDCE93B535831E02AF141379EB3FBAC3C811310904180A6C135719B7683FD"
 
-Write-Host "Downloading WSL Kernel.`n"
+Write-Host "Downloading WSL Kernel and installing.`n"
 # This is just preference for the most part
 cd ~\desktop
 Invoke-Webrequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile wsl_update_x64.msi
-Invoke-Item .\wsl_update_x64.msi
-Write-Host "Complete the install before continuing."
-Pause
+msiexec /package wsl_update_x64.msi /passive /norestart
 
 Write-Host "Setting Default WSL Version to 2."
 wsl.exe --set-default-version 2
 
-# This speeds up downloads in certain versions of powershell
-$ProgressPreference = 'SilentlyContinue'
 Write-Host "`nDownloading root filesystem. Progress will not be displayed to improve download speed.`n"
 # Add dl=1 to download from cmd line when using dropbox
 iwr $rootFS -o $outFile
-$ProgressPreference = 'Continue'
 $dlHash = Get-FileHash -Algorithm sha256 $outFile | select Hash
 Write-Host "Original file hash: " $kaliHash
 Write-Host "Download file hash: " $dlHash.Hash
@@ -49,4 +45,6 @@ Write-Host "Converting to WSL version 2."
 # Comment out the next line if you prefer WSL 1, it will get you started considerably faster
 wsl --set-version kali-SRT 2
 wsl --list --verbose
-Write-Host "`nYou should see kali above and in a stopped state if this script worked." -ForegroundColor Cyan
+Write-Host "`nYou should see kali above and in a stopped state if this script worked. 
+Run wsl, and follow the following guide to configure your interfaces if you are using WSL2: 
+https://support.synack.com/hc/en-us/articles/10656258703379-LP-Setting-up-Kali-via-WSL2" -ForegroundColor Cyan

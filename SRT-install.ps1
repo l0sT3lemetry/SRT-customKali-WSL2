59fad5f7dc234d02b9b8047a223c0395e0fbb67c
l0sT3lemetry <145846586+l0sT3lemetry@users.noreply.github.com>
2023-11-21 22:18:29 +0900

    Update SRT-install.ps1

--- SRT-install.ps1
+++ SRT-install.ps1
@@ -10,11 +10,13 @@ root CA installed
 
 the ability to download
 #>
+$kaliHash = "8A8BDCE93B535831E02AF141379EB3FBAC3C811310904180A6C135719B7683FD"
 
 Write-Host "Attempting to install."
 cd ~\desktop
 
 Invoke-Webrequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile wsl_update_x64.msi
+Start-Sleep -Seconds 1
 Invoke-Item .\wsl_update_x64.msi
 Write-Host "Complete the install before continuing."
 Pause
@@ -27,6 +29,10 @@ wsl.exe --set-default-version 2
 Write-Host "Downloading root filesystem."
 # Add dl=1 to download from cmd line when using dropbox
 iwr https://www.dropbox.com/s/<redacted>/kali-linux-rolling-wsl-rootfs-amd64.tar.gz?dl=1 -o kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+$dlHash = Get-FileHash -Algorithm sha256 .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+$kaliHash
+$dlHash.Hash
+
 Write-Host "Downloading LxRunOffline."
 iwr https://github.com/DDoSolitary/LxRunOffline/releases/download/v3.5.0/LxRunOffline-v3.5.0-msvc.zip -o lxrun.zip
 
@@ -39,9 +45,9 @@ Start-Sleep -Seconds 1
 .\lxrun\LxRunOffline.exe install -n kali -d .\kali\ -f .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
 
 # more cleanup
-#Remove-Item .\lxrun.zip
-#Remove-Item .\lxrun -Recurse
-#Remove-Item .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
+Remove-Item .\lxrun.zip
+Remove-Item .\lxrun -Recurse
+Remove-Item .\kali-linux-rolling-wsl-rootfs-amd64.tar.gz
 
 wsl --list --verbose
 Write-Host "You should see kali above and in a stopped state if this script worked."
